b418b478e7497cc8d38f15abc0a8809daf9fb455
Carl Antuar <carl.antuar@smartservice.qld.gov.au>
2021-06-11 16:00:00 +1000

    [QOL-8059] initial Github Actions workflow

---
+++ .docker/scripts/create-test-data.sh
@@ -0,0 +1,33 @@
+
+#!/usr/bin/env sh
+##
+# Create some example content for extension BDD tests.
+#
+set -e
+
+CKAN_ACTION_URL=http://ckan:3000/api/action
+
+if [ "$VENV_DIR" != "" ]; then
+  . ${VENV_DIR}/bin/activate
+fi
+
+# We know the "admin" sysadmin account exists, so we'll use her API KEY to create further data
+API_KEY=$(ckan_cli user admin | tr -d '\n' | sed -r 's/^(.*)apikey=(\S*)(.*)/\2/')
+
+# Creating test data hierarchy which creates organisations assigned to datasets
+ckan_cli create-test-data hierarchy
+
+# Creating basic test data which has datasets with resources
+ckan_cli create-test-data
+
+echo "Updating annakarenina to use department-of-health Organisation:"
+package_owner_org_update=$( \
+    curl -LsH "Authorization: ${API_KEY}" \
+    --data "id=annakarenina&organization_id=department-of-health" \
+    ${CKAN_ACTION_URL}/package_owner_org_update
+)
+echo ${package_owner_org_update}
+
+if [ "$VENV_DIR" != "" ]; then
+  deactivate
+fi
