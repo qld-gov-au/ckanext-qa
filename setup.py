7b39f36ba69c25339ce8809bb11de3d75b71049b
Dushko Bogdanovski <dule.bogdanovski@gmail.com>
2022-05-10 16:14:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'ckanext-archiver>=2.0',
+        'ckanext-archiver',
         'ckanext-report',
         'SQLAlchemy>=0.6.6',
         'requests',
